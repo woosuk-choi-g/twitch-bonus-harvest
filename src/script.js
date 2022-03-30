c372aaa369e61f477977e683fef3e017eb8cdb08
Rogal3 <cws0188@gmail.com>
2022-03-30 22:53:14 +0900

    version 1.0: add timer

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         보너스 수확기
+// @name         트위치 보너스 수확기
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  twitch-bonus-harvest
 // @author       You
 // @match        https://www.twitch.tv/*
@@ -9,50 +9,96 @@
 // @grant        none
 // ==/UserScript==
 
-const settings = {
-  title: "보너스 수확기",
-  query: '[aria-label="보너스 받기"]',
-  interval: 1000,
-  healthCheckCount: 60,
-  count: 0,
-  enable: true,
-}
-
-function stop() {
-  settings.enable = false;
-}
-
-function now() {
-  const today = new Date();
-  today.setHours(today.getHours() + 9);
-  return today.toISOString().replace('T', ' ');
-}
-
-function logNowAndReturn(obj) {
-  console.log(`found bonus ${now()}`);
-  return obj;
-}
-
-function click(obj) {
-  obj.click();
-  return obj;
-}
-
-function run(settings) {
-  console.log(`${settings.title} start ${now()}`);
-  const interval = setInterval(() => {
-      if (!settings.enable) {
-          clearInterval(interval);
-      }
+(() => {
+  const settings = {
+    title: "보너스 수확기",
+    query: '[aria-label="보너스 받기"]',
+    enable: true,
+    boxCoolTime: 15, // minutes
+  }
+  
+  const timerStyle = {
+    "display": "inline-flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "margin-right": "auto",
+    "color": "pink"
+  };
+  
+  const MILLISECOND = 1;
+  const SECOND = 1000 * MILLISECOND;
+  const MINUTE = 60 * SECOND;
+  
+  class Timer {
+    constructor(minutes, seconds=0) {
+      this.reset(minutes, seconds);
+    }
+  
+    reset(minutes, seconds=0) {
+      this.inner = minutes * MINUTE;
+      this.inner += seconds * SECOND;
+    }
+  
+    toString() {
+      const minutes = Math.floor(this.inner / MINUTE);
+      const seconds = Math.floor(this.inner % MINUTE / SECOND);
+      const formmatedSeconds = seconds > 9 ? "" + seconds : "0" + seconds;
+      return `${minutes}:${formmatedSeconds}`;
+    }
+  
+    subtract(seconds=1) {
+      this.inner -= seconds * SECOND;
+      return this.inner;
+    }
+  }
+  
+  function now() {
+    const today = new Date();
+    today.setHours(today.getHours() + 9);
+    return today.toISOString().replace('T', ' ');
+  }
+  
+  function makeTimer() {
+    const e = document.createElement("div");
+    Object.entries(timerStyle)
+      .map(([propertyName, propertyValue]) => e.style.setProperty(propertyName, propertyValue));
+    const containerList = document.getElementsByClassName("chat-input__buttons-container");
+    console.log(containerList);
+    if (!containerList) {
+      return e;
+    }
+    const container = containerList[0];
+    const before = container.firstChild;
+    before.after(e);
+    return e;
+  }
+  
+  function logNowAndReturn(obj) {
+    console.log(`found bonus ${now()}`);
+    return obj;
+  }
+  
+  function click(obj) {
+    obj.click();
+    return obj;
+  }
+  
+  function run(settings) {
+    console.log(`${settings.title} start ${now()}`);
+    const timerElement = makeTimer();
+    const timer = new Timer(settings.boxCoolTime);
+    const interval = setInterval(() => {
+      timer.subtract();
+      timerElement.innerText = timer.toString();
       const nodeList = document.querySelectorAll(settings.query);
       Array.from(nodeList.values())
           .map(logNowAndReturn)
           .map(click);
-      // if (config.count % config.healthCheckCount == 0) {
-      //     console.log(`${config.title} ${now()}`);
-      // }
-      // config.count++;
-  }, settings.interval);
-}
-
-run(settings);
+      if (nodeList.length) {
+        timer.reset(settings.boxCoolTime);
+      }
+    }, SECOND);
+  }
+  
+  run(settings);
+})();
